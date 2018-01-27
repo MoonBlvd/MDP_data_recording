21f301ada374e7027970a8949fca209ab8506d38
MoonBlvd <brianyao@umich.edu>
2018-01-27 15:53:51 -0500

    cmd line params

--- compressor.py
+++ compressor.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 import time
 import numpy as np
+import sys
 
 class simpleCompress():
     def __init__(self):
@@ -17,7 +18,7 @@ class simpleCompress():
     def run_opencv(self,img,quality,ext,qual_param):
         cv2.imwrite('000000'+ext, img, [qual_param, quality])
         new_size = os.stat(os.path.join(os.getcwd(), '000000'+ext)).st_size/1024
-        os.remove(os.path.join(os.getcwd(), '000000'+ext))
+        #os.remove(os.path.join(os.getcwd(), '000000'+ext))
         return new_size
     def run_opencv_encoder(self,img,quality,ext,qual_param):
         _,compressed = cv2.imencode(ext, img, [qual_param, quality])
@@ -32,7 +33,7 @@ if __name__ == '__main__':
     quality_list = [10,20,30,40,50,60,70,80,90,100]
     level_list = [0,1,2,3,4,5,6,7,8,9]
     binary_list = [0,1]
-    ext = '.webp'
+    ext = sys.argv[1]
 
     start_time = time.time()
     print ('Compression is: '+ext)
@@ -58,12 +59,13 @@ if __name__ == '__main__':
         # elapsed_time = time.time() - start_time
         # print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
         
-        #start_time = time.time()
-        #new_size = compressor.run_opencv(img, quality,ext)
-        #elapsed_time = time.time() - start_time
-        #print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
+        start_time = time.time()
+        new_size = compressor.run_opencv(img, quality,ext, qual_param)
+        elapsed_time = time.time() - start_time
+        print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
 
         start_time = time.time()
         new_size = compressor.run_opencv_encoder(img, quality,ext, qual_param)
         elapsed_time = time.time() - start_time
-        print ("OpenCV: " + str(format(new_size,'.4f')) + "%; Time: " + str(format(elapsed_time,'.4f')) + 'second')
+        print ("OpenCV: " + str(format(new_size,'.4f')) + "\%; Time: " + str(format(elapsed_time,'.4f')) + 'second')
+        break
