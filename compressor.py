922de2fa93843dea0a964d0e887886c8308ef89a
MoonBlvd <brianyao@umich.edu>
2018-05-06 02:04:31 -0400

    changed curve fitting, pdf fitting and cutin computation

--- compressor.py
+++ compressor.py
@@ -6,6 +6,7 @@ import time
 import numpy as np
 import sys
 import csv
+import glob
 
 def write_csv(file_path, data):
     with open(file_path, 'w') as csvfile:
@@ -59,19 +60,18 @@ class simpleCompress():
 
 if __name__ == '__main__':
     compressor = simpleCompress('recorded_img/')
-    img = cv2.imread('test_image.png')
+    # img = cv2.imread('test_image.png')
 
-    video_path = '../Smart_Black_Box/data/videos/'
-    video_name = '05182017_video1080p.mp4'
-    cap = cv2.VideoCapture(video_path + video_name)
+    file_path = '../Smart_Black_Box/data/Data_10172017/videos/*'
+    all_files = glob.glob(file_path)
 
-    quality_list = [i for i in range(101)]#[10,20,30,40,50,60,70,80,90,100]
-    level_list = [9,8,7,6,5,4,3,2,1,0]
-    binary_list = [0,1]
     ext = sys.argv[1]
+    F = open('compression_ratio_quality_WV.csv', 'w')
+    writer = csv.writer(F)
 
-    start_time = time.time()
-    print ('Compression is: '+ext)
+    quality_list = [i for i in range(101)]  # [10,20,30,40,50,60,70,80,90,100]
+    level_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    binary_list = [0, 1]
 
     if ext == '.jpeg':
         param_list = quality_list
@@ -82,43 +82,52 @@ if __name__ == '__main__':
     elif ext == '.png':
         param_list = level_list
         qual_param = cv2.IMWRITE_PNG_COMPRESSION
-    elif ext == '.ppm' or '.pgm'or '.pbm':
+    elif ext == '.ppm' or '.pgm' or '.pbm':
         param_list = binary_list
         qual_param = cv2.IMWRITE_PXM_BINARY
     else:
         raise NameError('Extension name not recognized')
 
-    frame_ctr = 0
-    frames_to_watch = np.random.permutation(50000)
-    #while cap.isOpened():
-    all_output = []
-    F = open('compression_ratio_quality.csv','w')
-    writer = csv.writer(F)
-    for img_id in frames_to_watch[:100]:
+    for video_file in all_files:
 
-        cap.set(1, img_id)
-        ret,img = cap.read()
+        # video_path = '../Smart_Black_Box/data/videos/'
+        # video_name = '05182017_video1080p.mp4'
 
-        frame_ctr += 1
-        all_quality = []
-        all_compress_ratio = []
-        for i,quality in enumerate(param_list):
-        # start_time = time.time()
-        # new_size = compressor.run_opencv(img, quality,ext,qual_param)
-        # elapsed_time = time.time() - start_time
-        # print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
-        
-        # start_time = time.time()
-        # new_size = compressor.run_opencv(img, quality,ext, qual_param,i)
-        # elapsed_time = time.time() - start_time
-        # print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
+        cap = cv2.VideoCapture(video_file)
 
-            start_time = time.time()
-        # new_size = compressor.run_opencv_encoder(img, ext, qual_param,quality=quality)
-            new_size,compress_ratio = compressor.run_opencv(img,ext,qual_param,quality=quality,i=0,j=0,a=0,persistent_record=False)
-            elapsed_time = time.time() - start_time
-            print ("OpenCV: " + str(format(new_size,'.4f')) + "\%; Time: " + str(format(elapsed_time,'.4f')) + 'second')
-            print ("Ratio: " + str(compress_ratio) + "  Quality: " + str(quality))
-            #all_quality.append(quality)
-            #all_compress_ratio.append(compress_ratio)
-            writer.writerow([compress_ratio,quality])
+
+        start_time = time.time()
+        # print ('Compression is: '+ext)
+
+        frame_ctr = 0
+        frames_to_watch = np.random.permutation(1700)
+        #while cap.isOpened():
+        all_output = []
+
+        for img_id in frames_to_watch[:2]:
+
+            cap.set(1, img_id)
+            ret,img = cap.read()
+
+            frame_ctr += 1
+
+            for i,quality in enumerate(param_list):
+            # start_time = time.time()
+            # new_size = compressor.run_opencv(img, quality,ext,qual_param)
+            # elapsed_time = time.time() - start_time
+            # print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
+
+            # start_time = time.time()
+            # new_size = compressor.run_opencv(img, quality,ext, qual_param,i)
+            # elapsed_time = time.time() - start_time
+            # print ("OpenCV: ", new_size, "KB; Time: ",elapsed_time, 'second')
+
+                start_time = time.time()
+            # new_size = compressor.run_opencv_encoder(img, ext, qual_param,quality=quality)
+                new_size,compress_ratio = compressor.run_opencv(img,ext,qual_param,quality=quality,i=0,j=0,a=0,persistent_record=False)
+                elapsed_time = time.time() - start_time
+                print ("OpenCV: " + str(format(new_size,'.4f')) + "\%; Time: " + str(format(elapsed_time,'.4f')) + 'second')
+                print ("Ratio: " + str(compress_ratio) + "  Quality: " + str(quality))
+                #all_quality.append(quality)
+                #all_compress_ratio.append(compress_ratio)
+                writer.writerow([compress_ratio,quality])
